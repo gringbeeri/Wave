4f73f62da51fa5edfec26d94882442aa0688607d
Aloha friends <68374649+gringbeeri@users.noreply.github.com>
2020-12-26 01:35:22 +0300

    New file

--- second_chapter.py
+++ second_chapter.py
@@ -1,137 +1,318 @@
 #1
-name = input("Введите ваше имя: ")
-city = input("Введите ваш город: ")
-region = input("Введите ваш регион ")
-index = int(input("Введите ваш индекс: "))
-number = int(input("Введите ваш номер телефона: "))
-specialty = input("Введите вашу специальность: ")
+number = int(input('Введите число: '))
 
-print(f"Вас зовут {name}")
-print(f"Вы проживаете в городе {city}, {region} область, ваш почтовый индекс: {index}")
-print(f"Ваш номер телефона: {number}")
-print(f"Ваша специальность: {specialty}")
+if number == 1:
+    print('Понедельник')
+elif number == 2:
+    print('Вторник')
+elif number == 3:
+    print('Среда')
+elif number == 4:
+    print('Четверг')
+elif number == 5:
+    print('Пятница')
+elif number == 6:
+    print('Суббота')
+elif number == 7:
+    print('Воскресенье')
+else:
+    print('Такого дня не сущетсвует!')
 
 #2
-summa = int(input("Введите сумму: "))
-result = (summa * 23) / 100
-print(result)
+len_triangle_1 = int(input('Введите длину: '))
+width_triangle_1 = int(input('Введите ширину: '))
+result_1 = len_triangle_1 * width_triangle_1
+print(f'Площадь треугольника равна: {result_1}')
+
+len_triangle_2 = int(input('Введите длину: '))
+width_triangle_2 = int(input('Введите ширину: '))
+result_2 = len_triangle_2 * width_triangle_2
+print(f'Площадь треугольника равна: {result_2}')
+
+if result_1 > result_2:
+    print('Площадь первого треугольника больше')
+elif result_1 < result_2:
+    print('Площадь второго треугольника больше')
+else:
+    print('Стороны равны')
 
 #3
-quantity = int(input("Введите количество квадратных метров: "))
-result = quantity / 4047
-print(result)
+age = float(input("Введите ваш возраст: "))
+
+if age <= 1:
+    print('Младенец')
+elif age > 1 and age <= 13:
+    print('Ребенок')
+elif age > 13 and age < 20:
+    print('Подросток')
+else:
+    print('Взрослый')
 
 #4
-product_1 = int(input("Сумма 1 товара: "))
-product_2 = int(input("Сумма 2 товара: "))
-product_3 = int(input("Сумма 3 товара: "))
-product_4 = int(input("Сумма 4 товара: "))
-product_5 = int(input("Сумма 5 товара: "))
+numbers = int(input('Введите число: '))
 
-summa = product_1 + product_2 + product_3 + product_4 + product_5
-print(summa)
-tax_summa = (summa * 7) // 100
-print(tax_summa)
-result = summa + int(tax_summa)
-print(result)
+if numbers == 1:
+    print('I')
+elif numbers == 2:
+    print('II')
+elif numbers == 3:
+    print('III')
+elif numbers == 4:
+    print('IV')
+elif numbers == 5:
+    print('V')
+elif numbers == 6:
+    print('VI')
+elif numbers == 7:
+    print('VII')
+elif numbers == 8:
+    print('VIII')
+elif numbers == 9:
+    print('IX')
+elif numbers == 10:
+    print('X')
+else:
+    print('Не существует')
 
 #5
-speed = 70
-distance_1 = speed * 6
-print(distance_1)
-distance_2 = speed * 10
-print(distance_2)
-distance_3 = speed * 15
-print(distance_3)
+body_mass = int(input('Введите массу тела: '))
+body_weigh = body_mass * 9.8
+print(body_weigh)
+
+if body_weigh < 100:
+    print('Слишком маленький вес')
+elif body_weigh > 500:
+    print('Слишком большой вес')
 
 #6
-money = int(input("Введите сумму средств: "))
-print(money)
-federal  = (money * 5) // 100
-print(federal)
-regional = (money * 2.5) // 100
-print(int(regional))
-general_tax = federal + int(regional)
-print(general_tax)
-result = money - general_tax
-print(result)
+day = int(input('Введите день: '))
+month = input('Введите месяц: ')
+year = int(input('Введите год: '))
+print(f'{day}.{month}.{year}')
 
-#7
-kilometers = int(input("Введите число пройденного пути: "))
-petrol = float(input("Количество литров: "))
-result = kilometers // petrol
-print(result)
+if day * int(month) == year:
+    print('Ваш год волшебный!')
+else:
+    print('Обычный год')
 
-#8
-summa = float(input("Введите сумму: "))
-print(summa)
-tip = (summa * 18) // 100
-print(tip)
-tax = (summa * 7) // 100
-print(tax)
-summa_tax = tip + tax
-print(summa_tax)
-result = summa - summa_tax
-print(result)
+# 7
+first_color = input('Введите первый цвет: ')
+second_color = input('Введите второй цвет: ')
+
+if first_color == 'красный' and second_color == 'синий':
+    print('Фиолетовый')
+elif first_color == 'желтый' and second_color == 'красный':
+    print('Оранжевый')
+elif first_color == 'синий' and second_color == 'желтый':
+    print('Зеленый')
+else:
+    print('Неверные цвета')
+
+8
+import math
+qty_people = int(input('Количетсво участников: '))
+qty_hot_gog = int(input('Количетсво хот-догов на человека: '))
+sausage = 8
+bun = 10
+
+general_qty = qty_people * qty_hot_gog
+print(f'Общее количество хот-догов: {general_qty}')
+result_sausage = general_qty / sausage
+qty_sausage = math.ceil(result_sausage) * sausage
+print(f'Минимальное количество упаковок сосисок: {math.ceil(result_sausage)}')
+result_bun = general_qty / bun
+qty_bun = math.ceil(result_bun) * bun
+print(f'Минимальное количество упаковок булок: {math.ceil(result_bun)}')
+result_sausage_1 = qty_sausage % general_qty
+print(f'Оставшиеся сосиски: {result_sausage_1}')
+result_bun_1 = qty_bun % general_qty
+print(f'Оставшиеся булки: {result_bun_1}')
 
 #9
-temperature = int(input("Введите температуру: "))
-temperature_F = int(((9 / 5) * temperature) + 32)
-print(temperature_F)
+pocket = int(input('Введите номер кармана: '))
 
-#10
+if pocket > 36:
+    print('Ошибка')
+elif pocket == 0:
+    print('Карман зеленый')
+elif pocket >= 1 and pocket <= 10:
+    if pocket % 2 == 0:
+        print('Карман черный')
+    elif pocket % 2 != 0:
+        print('Карман красный')
+elif pocket >= 11 and pocket <= 18:
+    if pocket % 2 == 0:
+        print('Карман красный')
+    elif pocket % 2 != 0:
+        print('Карман черный')
+elif pocket >= 19 and pocket <= 28:
+    if pocket % 2 == 0:
+        print('Карман черный')
+    elif pocket % 2 != 0:
+        print('Карман красный')
+elif pocket >= 29 and pocket <= 36:
+    if pocket % 2 == 0:
+        print('Карман красный')
+    elif pocket % 2 != 0:
+        print('Карман черный')
+
+# 10
+summa = 0
+first_try = int(input('Введите монету: '))
+summa += first_try
+second_try = int(input('Введите монету: '))
+summa += second_try
+third_try = int(input('Введите монету: '))
+summa += third_try
+
+if summa == 100:
+    print(f'Достаточная сумма - {summa}')
+elif summa < 100:
+    print(f'Маленькая сумма - {summa}')
+elif summa > 100:
+    print(f'Большая сумма - {summa}')
 
 
 #11
-men = int(input("Количество учащихся мужского пола: "))
-female = int(input("Количество учащихся мужского пола: "))
-qty = men + female
-percent_men = men // qty
-print(percent_men)
-percent_female = female // qty
-print(percent_female)
+point = 0
+books = int(input('Введите количество книг: '))
+
+if books == 0:
+    point += 0
+    print(f'У вас {point} очков')
+elif books == 2:
+    point += 5
+    print(f'У вас {point} очков')
+elif books == 4:
+    point += 15
+    print(f'У вас {point} очков')
+elif books == 6:
+    point += 30
+    print(f'У вас {point} очков')
+elif books > 8:
+    point += 60
+    print(f'У вас {point} очков')
 
 #12
-number_shares_buy = int(input("Число купленных акций: "))
-buy_shares = float(input("Сумма за покупку акции: "))
-result_buy_shares = number_shares_buy * buy_shares
-procent_shares_buy = 3
-procent_buy = (result_buy_shares * procent_shares_buy) // 100
-result_buy = result_buy_shares - procent_buy
-print(int(result_buy))
+software = int(input('Введите число приобретенных пакетов: '))
+software_money = 99
 
-number_shares_sale = int(input("Число проданных акций: "))
-sale_shares = float(input("Сумма за продажу акции: "))
-result_sale_shares = number_shares_sale * sale_shares
-procent_shares_sale = 3
-procent_sale = (result_sale_shares * procent_shares_sale) // 100
-result_sale = result_sale_shares - procent_sale
-print(int(result_sale))
-
-result_shares = result_sale - result_buy
-print(f'Итоговая сумма выигрыша: {result_shares}')
-
+if software < 9:
+    result = software * software_money
+    print(f'Сумма за покупку без скидки: {result}$')
+elif software >= 10 and software <= 19:
+    summa = software * software_money
+    discount = (summa * 10) // 100
+    result = summa - discount
+    print(f'Сумма за покупку со скидки: {result}$')
+elif software >= 20 and software <= 49:
+    summa = software * software_money
+    discount = (summa * 20) // 100
+    result = summa - discount
+    print(f'Сумма за покупку со скидки: {result}$')
+elif software >= 50 and software <= 99:
+    summa = software * software_money
+    discount = (summa * 30) // 100
+    result = summa - discount
+    print(f'Сумма за покупку со скидки: {result}$')
+elif software >= 100:
+    summa = software * software_money
+    discount = (summa * 40) // 100
+    result = summa - discount
+    print(f'Сумма за покупку со скидки: {result}$')
+   
 #13
-length_vines = float(input("Длина лоз: "))
-volume_space_prop = float(input("Объем пространства опор: "))
-volume_space_vines = float(input("Объем пространства лоз: "))
-result_vines = (length_vines - 2) * volume_space_prop)) / volume_space_vines
-print(int('Количество виноградных лоз: {result_vines}'))
+summa = 0
+weight_package = int(input('Введите массу пакета: '))
+
+if weight_package <= 200:
+    summa = 150
+    print(f'Сумма доставки равна: {summa}')
+elif weight_package > 200 and weight_package < 599:
+    summa = 300
+    print(f'Сумма доставки равна: {summa}')
+elif weight_package >= 600 and weight_package < 999:
+    summa = 400
+    print(f'Сумма доставки равна: {summa}')
+elif weight_package >= 100:
+    summa = 475
+    print(f'Сумма доставки равна: {summa}')
 
 #14
-basic_summa = float(input("Внесенная основная сумма: "))
-annual_rate = float(input("Годовая ставка: "))
-frequency = int(input("Частота начисления суммы: "))
-years = int(input("Количество лет: "))
+weight = float(input('Введите вашу массу: '))
+growth = float(input('Введите ваш рост: '))
+body_index = weight / (growth ** 2)
+body_index = float('{:.1f}'.format(body_index))
+print(body_index)
 
-result_summa = basic_summa * ((1 + (annual_rate / frequency)) ** 2)
-print(f'Итоговый баланс: {int(result_summa)}')
+if body_index <= 18.5:
+    print('Ваш индекс массы недостаточный')
+elif body_index >= 18.5 and body_index <= 25:
+    print('Ваш индекс массы оптимальный')
+elif body_index > 25:
+    print('Ваш индекс массы превышен')
 
 #15
-#Черепашья графика - я пока выполняю только по коду, графику изучу позже - если можно.
+# time = int(input('Введите количество секунд: '))
+# minute = time // 60 
+# hour = time * 60 * 60
+# day = 24 * 60 * 60
 
+# if time >= 60 and time < 3599:
+#     minute = time // 60 
+#     second = minute % 60
+#     print(f'{minute} минут, {second} секунд')
+# elif time >= 3600 and time < 86399:
+#     hour = second * 60 * 60
+#     print(f'{hour} часов, {minute} минут, {tume} секунд')
+# elif time >= 86400:
+#     print(f'{day} дня, {hour} часов, {minute} минут, {time} секунд')
 
+16
+year = int(input('Введите год: '))
 
+if year % 100 == 0 and year % 400 == 0:
+    print(f'В {year} году, в феврале было 29 дней')
+elif year % 100 != 0 and year % 4 == 0:
+    print(f'В {year} году, в феврале было 29 дней')
+else:
+    print(f'{year} - год не високосный')
 
+17
+internet = input('У вас плохое Wi-Fi соединение? ')
 
+if internet == 'да':
+    print('Пробовали перезагрузить устройство? ')
+    internet = input('Ваш ответ: ')
+    if internet == 'да':
+        print('Хорошо, вытащите кабель интернета из порта роутера и снова вставьте, затем перезгрузите роутер! Решилось?')
+        internet = input('Ваш ответ: ')
+        if internet == 'нет':
+            print('Рекомендуем заменить роутер')
+        elif internet == 'да':
+            print('Отлично, поздравляем')
+    elif internet == 'нет':
+        print('Перезагрузитесь пожалуйста')
+elif internet == 'нет':
+    print('Рады что все хорошо')
 
+#18
+vegetarians = ['Центральная пицерия', 'Кафе за углом', 'Блюдо от итальняской мамы', 'Кухня шеф-повар']
+vegans = ['Кафе за углом', 'Кухня шеф-повар']
+diets = ['Центральная пицерия', 'Кафе за углом', 'Кухня шеф-повар']
+vegetarians_vegans = ['Кафе за углом', 'Кухня шеф-повар']
+vegetarians_diets = ['Центральная пицерия', 'Кафе за углом', 'Кухня шеф-повар']
+vegans_diets = ['Кафе за углом', 'Кухня шеф-повар']
+vegetarians_vegans_diets = ['Центральная пицерия', 'Кафе за углом', 'Блюдо от итальняской мамы', 'Кухня шеф-повар']
+
+vegetarian = input('Есть ли среди группы вегетарианцы? ')
+vegan = input('Есть ли среди группы веганы? ')
+diet = input('Есть ли среди группы люди с диетой? ')
+if vegetarian == 'да' or vegan == 'да' and diet == 'нет':
+    print('\n'.join(vegetarians_vegans))
+elif vegetarians == 'да' and vegan == 'нет' or diet == 'да':
+    print('\n'.join(vegetarians_diets))
+elif vegetarians == 'нет' and vegan == 'да' or diet == 'да':
+    print('\n'.join(vegans_diets))
+elif vegetarians == 'да' and vegan == 'да' and diet == 'да':
+    print('\n'.join(vegetarians_vegans_diets))
