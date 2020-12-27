4bd135f869900bb7ab206ab5f156797a3fafd7bd
gringbeeri <vladgashko@gmail.com>
2020-12-28 00:28:21 +0300

    news file

---
+++ chapter/chapter_2_8.py
@@ -0,0 +1,10 @@
+#8
+summa = float(input("Введите сумму: "))
+tip = summa * 0.18
+print(tip)
+tax = summa * 0.7
+print(tax)
+summa_tax = tip + tax
+print(summa_tax)
+result = summa - summa_tax
+print(result)
