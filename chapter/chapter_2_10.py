c20006c144c26a83b010d62a9ae20b2edd16cd1e
gringbeeri <vladgashko@gmail.com>
2021-01-04 21:35:01 +0300

    addition

--- chapter/chapter_2_10.py
+++ chapter/chapter_2_10.py
@@ -1,1 +1,14 @@
 #10
+qty_bun = int(input('Количество булок: '))
+
+sugar = 1.5/48
+oil = 1/48
+flour = 2.75/48
+
+result_sugar = qty_bun * sugar
+result_oil = qty_bun * oil
+result_flour = qty_bun * flour
+
+print(f'Количество стаканов сахара: {result_sugar}')
+print(f'Количество стаканов масла: {result_oil}')
+print(f'Количество стаканов муки: {result_flour}')
