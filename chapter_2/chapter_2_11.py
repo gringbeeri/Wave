ab0f68972187dd29586fc1b4f19ee2af1528f326
Aloha friends <68374649+gringbeeri@users.noreply.github.com>
2021-01-04 18:26:09 +0300

    Delete.py

--- chapter_2/chapter_2_11.py
+++
@@ -1,8 +0,0 @@
-#11
-men = int(input("Количество учащихся мужского пола: "))
-female = int(input("Количество учащихся женского пола: "))
-qty = men + female
-percent_men = men / qty
-print(f'Процентность мужского пола: {percent_men}')
-percent_female = female / qty
-print(f'Процентность женского пола: {percent_female}')
