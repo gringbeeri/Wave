b9f882a71f7d6609ff22ce554f820dda7dafaed9
gringbeeri <vladgashko@gmail.com>
2021-01-06 22:12:07 +0300

    changed

--- chapter/chapter_2_11.py
+++ chapter/chapter_2_11.py
@@ -2,7 +2,7 @@
 men = int(input("Количество учащихся мужского пола: "))
 female = int(input("Количество учащихся женского пола: "))
 qty = men + female
-percent_men = men / qty
+percent_men = (men / qty) * 100
 print(f'Процентность мужского пола: {percent_men}')
-percent_female = female / qty
+percent_female = (female / qty) * 100
 print(f'Процентность женского пола: {percent_female}')
